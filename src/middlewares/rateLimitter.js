449befb9c022dc533fe8f08d2ea2036513cb3280
Arjun S Kumar <117592600+ArjunSKumar7@users.noreply.github.com>
2025-06-19 11:37:53 +0530

    Update rateLimitter to 1 min and 30 req

--- src/middlewares/rateLimitter.js
+++ src/middlewares/rateLimitter.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
  const rateLimiter = rateLimit({
-windowMs: 15 * 60 * 1000, 
-max: 10, 
+windowMs: 1 * 60 * 1000, 
+max: 30, 
 message: {
 status: 429,
 message: 'Too many attempts from this IP, please try again after 15 minutes',
